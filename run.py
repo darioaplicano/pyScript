a8ead102ec0b919e927dcbebbbf95979c402ae60
darioaplicano <alex_dario92@hotmail.com>
2015-06-23 00:37:42 -0600

    Problemas con el guardado de la información

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ class Example(QtGui.QMainWindow):
 
     def __init__(self):
         super(Example, self).__init__()
+        self.filename = ''
         self.initUI()
 
     def initUI(self):
@@ -16,18 +17,45 @@ class Example(QtGui.QMainWindow):
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(QtGui.qApp.quit)
 
-        self.toolbar = self.addToolBar('Exit')
+        openAction = QtGui.QAction(QtGui.QIcon('Images/openlogo-50.png'), 'Open', self)
+        openAction.setShortcut('Ctrl+O')
+        self.connect(openAction, QtCore.SIGNAL('triggered()'), self.showDialog)
+
+        saveAction = QtGui.QAction(QtGui.QIcon('Images/save.png'), 'Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        self.connect(saveAction, QtCore.SIGNAL('triggered()'), self.saveDocument)
+
+        self.toolbar = self.addToolBar('werever')
         self.toolbar.addAction(exitAction)
+        self.toolbar.addAction(openAction)
+        self.toolbar.addAction(saveAction)
 
         self.entrada_texto = QtGui.QTextEdit(self)
         self.entrada_texto.move(30, 100)
         self.entrada_texto.setFocus()
-        self.entrada_texto.setFixedWidth(740)
-        self.entrada_texto.setFixedHeight(470)
+        self.setCentralWidget(self.entrada_texto)
 
         self.setGeometry(0, 0, 800, 600)
-        self.setWindowTitle('pyScript')
+        self.setWindowTitle('pyScript: ')
         self.show()
+
+    def showDialog(self):
+        #Se captura el nombre del archivo a abrir
+        self.filename = QtGui.QFileDialog.getOpenFileName(self, 'Abrir archivo',
+                    '/home')
+        #Se define un neuvo titulo a la ventan de la aplicacion
+        self.setWindowTitle('pyScript:%s' %self.filename)
+        #Se abre el archivo y se
+        #desplega la informacion en el widget de
+        #edicion de texto
+        fname = open(self.filename)
+        data = fname.read()
+        self.entrada_texto.setText(data)
+
+    def saveDocument(self):
+        fname = open(self.filename, 'w')
+        fname.write(self.entrada_texto)
+        fname.close()
 
 def main():
     app = QtGui.QApplication(sys.argv)
